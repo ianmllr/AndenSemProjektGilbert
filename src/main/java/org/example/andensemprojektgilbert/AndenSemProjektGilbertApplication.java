8d355c319c4942d70fa85c2291386354cf5353c3
ianmllr <130236399+ianmllr@users.noreply.github.com>
2025-05-05 11:10:33 +0200

    Initial commit

---
+++ src/main/java/org/example/andensemprojektgilbert/AndenSemProjektGilbertApplication.java
@@ -0,0 +1,13 @@
+package org.example.andensemprojektgilbert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndenSemProjektGilbertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndenSemProjektGilbertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/andensemprojektgilbert/AndenSemProjektGilbertApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.andensemprojektgilbert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndenSemProjektGilbertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
