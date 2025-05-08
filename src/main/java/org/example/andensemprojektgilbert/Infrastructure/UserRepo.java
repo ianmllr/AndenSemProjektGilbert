da03f31d67a2c85eec98c7530d613ea6e4d984e1
SpeedosDK <88831904+SpeedosDK@users.noreply.github.com>
2025-05-08 11:29:33 +0200

    opret bruger pt 1.

--- src/main/java/org/example/andensemprojektgilbert/Infrastructure/UserRepo.java
+++ src/main/java/org/example/andensemprojektgilbert/Infrastructure/UserRepo.java
@@ -2,6 +2,7 @@ package org.example.andensemprojektgilbert.Infrastructure;
 
 import org.example.andensemprojektgilbert.Model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -16,5 +17,21 @@ public class UserRepo {
         String sql = "INSERT INTO user (name, password, email, sales, rating, role)";
         int result = jdbcTemplate.update(sql, user.getName(), user.getPassword(), user.getEmail(), user.getSales(), user.getRating(), user.getRole());
         return result == 1;
+    }
+    public User readUserByEmail(String email) {
+        String sql = "SELECT * FROM user WHERE email = ?";
+
+        try {
+            return jdbcTemplate.queryForObject(sql, new Object[]{email}, (rs, rowNum) -> {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+                user.setEmail(rs.getString("email"));
+                return user;
+            });
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 }
---
+++ src/main/java/org/example/andensemprojektgilbert/Service/UserService.java
@@ -0,0 +1,29 @@
+package org.example.andensemprojektgilbert.Service;
+
+import org.example.andensemprojektgilbert.Infrastructure.UserRepo;
+import org.example.andensemprojektgilbert.Model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+    private User user;
+
+
+    public boolean register(User user) {
+        if (userRepo.readUserByEmail(user.getEmail()) != null) {
+            return false;
+        }
+        String hashed = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+        user.setPassword(hashed);
+        return userRepo.createUser(user);
+    }
+
+
+}
