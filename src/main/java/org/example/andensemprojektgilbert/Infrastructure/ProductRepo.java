0bae7fef8ed24af42a5016c0ae1cf388a8de357d
ianmllr <130236399+ianmllr@users.noreply.github.com>
2025-05-16 20:09:47 +0200

    fixet getIds

--- src/main/java/org/example/andensemprojektgilbert/Infrastructure/ProductRepo.java
+++ src/main/java/org/example/andensemprojektgilbert/Infrastructure/ProductRepo.java
@@ -14,7 +14,7 @@ public class ProductRepo {
     private JdbcTemplate jdbcTemplate;
 
     private Integer getColorId(String color) {
-        String sql = "SELECT id from Color WHERE name = ?";
+        String sql = "SELECT idcolor from Color WHERE color = ?";
         return jdbcTemplate.queryForObject(sql, Integer.class, color);
     }
 
@@ -24,7 +24,7 @@ public class ProductRepo {
     }
 
     private Integer getConditionId(String condition) {
-        String sql = "SELECT id FROM ItemCondition WHERE name = ?";
+        String sql = "SELECT idcondition FROM gilbet.condition WHERE itemcondition = ?";
         return jdbcTemplate.queryForObject(sql, new Object[]{condition}, Integer.class);
     }
 
