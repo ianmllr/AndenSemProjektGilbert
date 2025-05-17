48465d6108243ea0f6530fe988aeddfbd5c3540c
ThomasKiszely <thomas@bohn-jacobsen.dk>
2025-05-17 08:08:59 +0200

    Ændret så favoritter virker - også tests

--- src/test/java/org/example/andensemprojektgilbert/FavoriteTest.java
+++ src/test/java/org/example/andensemprojektgilbert/FavoriteTest.java
@@ -15,42 +15,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class FavoriteTest {
-//    @Autowired
-//    private FavoriteService favoriteService;
-//
-//    @BeforeEach
-//    void setUp() {
-//        favoriteService.addFavorite(1, 1);
-//        favoriteService.addFavorite(1, 2);
-//    }
-//    @Test
-//    void testGetFavoritesSize() {
-//        List<Product> favorites = favoriteService.getFavorites(1);
-//        assertEquals(2, favorites.size());
-//    }
-//    @Test
-//    void testGetFavoritesIds() {
-//        List<Product> favorites = favoriteService.getFavorites(1);
-//       Product product = favorites.get(1);
-//        assertEquals(2, product.getId());
-//    }
-//    @Test
-//    void testGetFavoritesByIdSize() {
-//        List<Integer> favoritesId = favoriteService.getFavoriteIds(1);
-//        assertEquals(2, favoritesId.size());
-//    }
-//    @Test
-//    void testGetFavoritesByIdIds() {
-//        List<Integer> favoritesId = favoriteService.getFavoriteIds(1);
-//        for (Integer favoriteId : favoritesId) {
-//            System.out.println(favoriteId);
-//        }
-//        assertEquals(2, favoritesId.get(1));
-//    }
-//
-//    @AfterEach
-//    public void tearDown() {
-//        favoriteService.removeFavorite(1, 1);
-//        favoriteService.removeFavorite(1, 2);
-//    }
+    @Autowired
+    private FavoriteService favoriteService;
+
+    @BeforeEach
+    void setUp() {
+        favoriteService.addFavorite(1, 1);
+        favoriteService.addFavorite(1, 2);
+    }
+
+    @Test
+    void testGetFavoritesSize() {
+        List<Product> favorites = favoriteService.getFavorites(1);
+        assertEquals(2, favorites.size());
+    }
+    @Test
+    void testGetFavoritesIds() {
+        List<Product> favorites = favoriteService.getFavorites(1);
+       Product product = favorites.get(1);
+        assertEquals(2, product.getId());
+    }
+    @Test
+    void testGetFavoritesByIdSize() {
+        List<Integer> favoritesId = favoriteService.getFavoriteIds(1);
+        assertEquals(2, favoritesId.size());
+    }
+    @Test
+    void testGetFavoritesByIdIds() {
+        List<Integer> favoritesId = favoriteService.getFavoriteIds(1);
+        for (Integer favoriteId : favoritesId) {
+            System.out.println(favoriteId);
+        }
+        assertEquals(2, favoritesId.get(1));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        favoriteService.removeFavorite(1, 1);
+        favoriteService.removeFavorite(1, 2);
+    }
 }
